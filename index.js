7c57660e6c638b957139608dc2aef26b1f55dfa6
Eugene <eugeneayonga@gmail.com>
2022-06-12 12:45:44 +0300

    commit tree 8129d812ed982ee6a92076ff631212f410a69bc8

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+
+// WRITECARDS
+function writeCards(stringNames, eventName) {
+
+    const messages = [];
+   
+     for (let i = 0; i < stringNames.length; i++) {
+   
+       const messageX = `Thank you, ${stringNames[i]}, for the wonderful ${eventName} gift!`;
+   
+       messages.push(messageX);
+     }
+   
+     return messages;
+   
+}
+   
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'wedding'));
+
+
+
+
+// COUNTDOWN
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
+
+console.log(countDown(10));
